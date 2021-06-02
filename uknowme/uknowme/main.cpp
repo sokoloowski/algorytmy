3611e925641823f3120437d4d32728d197cbb4c0
CleverLord <68718873+CleverLord@users.noreply.github.com>
2021-06-02 03:54:15 +0200

    Dodano Bucket Sort

--- uknowme/uknowme/main.cpp
+++ uknowme/uknowme/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "Bar_sort.h"
+#include "Bucket_sort.cpp"
 using namespace std;
 
 
 int main() {
+
+    Bucket_sort::Bucket_sort_main();
+    cout << endl;
+
     Bar_sort::bar_sort_main();
 
 
